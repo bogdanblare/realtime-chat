863bc11078eee3885b694c976f0372da2ea59bbf
Bogdan <bicharaa10@gmail.com>
2023-08-14 16:09:47 +0300

    update

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <title>Realtime Chat</title>
+      <body className="bg-[#131316]">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
